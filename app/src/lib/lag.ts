63b065390d59719b8bd734d3d4e8c865e0379bd6
Tim <godlyfast@gmail.com>
2024-06-03 23:41:08 +0300

    refactor: Update lagger module to use JSON response instead of text

--- app/src/lib/lag.ts
+++ app/src/lib/lag.ts
@@ -1,25 +1,43 @@
-export async function fullInstall(set: {ip: string, upSpeed: number, downSpeed: number}[]) {
-    const response = await fetch("/api/lagger?mode=fullInstall", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(set),
-    });
-    return await response.json();
+export async function fullInstall(
+  set: { ip: string; upSpeed: number; downSpeed: number }[]
+) {
+  const response = await fetch("/api/lagger?mode=fullInstall", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(set),
+  });
+  return await response.json();
 }
 
 export async function fullDell() {
-    const response = await fetch('/api/lagger?mode=fullDell');
-    return await response.json();
+  const response = await fetch("/api/lagger?mode=fullDell");
+  return await response.json();
 }
 
 export async function showConfig() {
-    const response = await fetch('/api/lagger?mode=showConfig');
-    return await response.json();
+  const response = await fetch("/api/lagger?mode=showConfig");
+  return await response.json();
 }
 
-export async function lagIp({upQueueId, downQueueId, upSpeed, downSpeed, oldDownSpeed, oldUpSpeed}:{upQueueId: number, downQueueId: number, upSpeed: number, downSpeed: number, oldUpSpeed: number, oldDownSpeed: number}) {
-    const response = await fetch(`/api/lagger?mode=lagIp&upQueueId=${upQueueId}&upSpeed=${upSpeed}&downQueueId=${downQueueId}&downSpeed=${downSpeed}&oldUpSpeed=${oldUpSpeed}&oldDownSpeed=${oldDownSpeed}`);
-    return await response.json();
+export async function lagIp({
+  upQueueId,
+  downQueueId,
+  upSpeed,
+  downSpeed,
+  oldDownSpeed,
+  oldUpSpeed,
+}: {
+  upQueueId: number;
+  downQueueId: number;
+  upSpeed: number;
+  downSpeed: number;
+  oldUpSpeed: number;
+  oldDownSpeed: number;
+}) {
+  const response = await fetch(
+    `/api/lagger?mode=lagIp&upQueueId=${upQueueId}&upSpeed=${upSpeed}&downQueueId=${downQueueId}&downSpeed=${downSpeed}&oldUpSpeed=${oldUpSpeed}&oldDownSpeed=${oldDownSpeed}`
+  );
+  return await response.json();
 }
