7326ff56f954f177c2c0ebd6d581d7e3c6481469
Tim <godlyfast@gmail.com>
2024-06-10 15:29:07 +0300

    refactor: Remove subquest module
    The subquest module was removed from the pf-router. This module was no longer being used and was causing unnecessary overhead. Removing it improves the overall performance and reduces the codebase complexity.

--- pf-router/subquest.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'subquest' {
-    export function getSubDomains(
-      options: { host: string; rateLimit: string; dnsServer: string; dictionary: string},
-      callback: (err: any, results: string[]) => void
-    ): void;
-}
