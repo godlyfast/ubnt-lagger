ce1125a0648e9c6822d6e15b67584a2991dc7d90
Tim <godlyfast@gmail.com>
2025-12-17 23:46:37 +0200

    refactor(pf-router): switch VPN config from PL UDP to NL TCP

--- pf-router/scripts/remote/connect-vpn.sh
+++ pf-router/scripts/remote/connect-vpn.sh
@@ -4,7 +4,7 @@ START=0
 END=$(($1))
 TOADD=0
 
-if [ "$END" == '' ] ; then $END=1; fi;
+if [ "$END" == '' ]; then END=1; fi
 
 echo Starting $MAX conns
 
@@ -12,20 +12,26 @@ source /opt/vyatta/etc/functions/script-template
 
 configure
 
-for (( i=$START; i<=$END; i++ ))
-do
+for ((i = $START; i <= $END; i++)); do
 
-echo "Checking tunnel $i"
+  echo "Checking tunnel $i"
 
-if [ "$(show interfaces | grep vtun$i)" != '' ] ; then echo "Tunnel $i is online"; continue; fi;
+  if [ "$(show interfaces | grep vtun$i)" != '' ]; then
+    echo "Tunnel $i is online"
+    continue
+  fi
 
-set interfaces openvpn vtun$i config-file /config/auth/PL-ovpn-udp.ovpn;
-set interfaces openvpn vtun$i description 'PUREVPN PL UDP Tunnel';
-TOADD=$((TOADD+1));
+  set interfaces openvpn vtun$i config-file /config/auth/NL-ovpn-tcp.ovpn
+  set interfaces openvpn vtun$i description 'PUREVPN NL TCP Tunnel'
+  TOADD=$((TOADD + 1))
 
 done
 
-if [ "$TOADD" == 0 ] ; then echo "No tunnels to add"; exit; fi;
+if [ "$TOADD" == 0 ]; then
+  echo "No tunnels to add"
+  exit
+fi
 
 commit
 save
+
